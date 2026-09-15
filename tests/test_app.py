6af181405ea0a80455b35e20d948938b0c39e4a5
shaikhfarhan04 <shaikhfarhan7644@gmail.com>
2026-09-16 00:46:17 +0530

    Add GitHub Actions CI pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,37 @@
+import sys
+import os
+
+sys.path.insert(
+    0,
+    os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..")
+    )
+)
+
+from app.app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json["status"] == "healthy"
+
+
+def test_version():
+    client = app.test_client()
+
+    response = client.get("/version")
+
+    assert response.status_code == 200
+    assert response.json["version"] == "1.0.1"
